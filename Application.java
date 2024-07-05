5936be6b6bc53d2251d54cbf3a6c31ab4356d309
OmarAhmd10 <omar.ahmed3@ucalgary.ca>
2024-07-04 19:29:26 -0600

    End of Section D

--- Application.java
+++ Application.java
@@ -21,62 +21,84 @@ public class Application {
        double num2 = args.length > 2 ? Double.parseDouble(args[2]) : 0;
       // Performs the operation that was put on the command-line
        switch (operation) {
-           case "+":
+           case "add":
                System.out.println("Result: " + add(num1, num2));
                break;
-           case "-":
+           case "subtract":
                System.out.println("Result: " + subtract(num1, num2));
                break;
-           case "*":
+           case "multiply":
                System.out.println("Result: " + multiply(num1, num2));
                break;
-           case "/":
+           case "divide":
                if (num2 != 0) {
                    System.out.println("Result: " + divide(num1, num2));
                } else {
                    System.out.println("Cannot divide by zero");
                }
                break;
-           case "!":
+           case "factorial":
                System.out.println("Result: " + factorial((int) num1));
+               break;
+           case "pow":
+               System.out.println("Result: " + power(num1, num2));
+               break;
+           case "sqrt":
+               System.out.println("Result: " + sqrt(num1));
+               break;
+           case "log":
+               System.out.println("Result: " + log(num1));
+               break;
+           case "log10":
+               System.out.println("Result: " + log10(num1));
+               break;
+           case "sin":
+               System.out.println("Result: " + sin(num1));
+               break;
+           case "cos":
+               System.out.println("Result: " + cos(num1));
+               break;
+           case "tan":
+               System.out.println("Result: " + tan(num1));
                break;
            default:
                System.out.println("Invalid operation");
+               break;
        }
    }
    // Method for interactive mode
    public static void interactiveMode() {
        Scanner scanner = new Scanner(System.in);
        while (true) {
-           System.out.println("Enter operation (+, -, *, /, !, exit): ");
+           System.out.println("Enter operation (add, subtract, multiply, divide, factorial, pow, sqrt, log, log10, sin, cos, tan, exit): ");
            String operation = scanner.next().toLowerCase();
            if (operation.equals("exit")) {
                break;
            }
            // reads the value based on what operation was inputed
            switch (operation) {
-               case "+":
+               case "add":
                    System.out.println("Enter the first value:");
                    double num1Add = scanner.nextDouble();
                    System.out.println("Enter the second value:");
                    double num2Add = scanner.nextDouble();
                    System.out.println("Result: " + add(num1Add, num2Add));
                    break;
-               case "-":
+               case "subtract":
                    System.out.println("Enter the first value:");
                    double num1Subtract = scanner.nextDouble();
                    System.out.println("Enter the second value:");
                    double num2Subtract = scanner.nextDouble();
                    System.out.println("Result: " + subtract(num1Subtract, num2Subtract));
                    break;
-               case "*":
+               case "multiply":
                    System.out.println("Enter the first value:");
                    double num1Multiply = scanner.nextDouble();
                    System.out.println("Enter the second value:");
                    double num2Multiply = scanner.nextDouble();
                    System.out.println("Result: " + multiply(num1Multiply, num2Multiply));
                    break;
-               case "/":
+               case "divide":
                    System.out.println("Enter the first value:");
                    double num1Divide = scanner.nextDouble();
                    System.out.println("Enter the second value:");
@@ -87,13 +109,51 @@ public class Application {
                        System.out.println("Cannot divide by zero");
                    }
                    break;
-               case "!":
+               case "factorial":
                    System.out.println("Enter a value:");
                    double numberFactorial = scanner.nextDouble();
-                   System.out.println("Result: " + factorial(numberFactorial));
+                   System.out.println("Result: " + factorial((int) numberFactorial));
+                   break;
+               case "pow":
+                   System.out.println("Enter the base value:");
+                   double base = scanner.nextDouble();
+                   System.out.println("Enter the exponent value:");
+                   double exponent = scanner.nextDouble();
+                   System.out.println("Result: " + power(base, exponent));
+                   break;
+               case "sqrt":
+                   System.out.println("Enter a value:");
+                   double valueSqrt = scanner.nextDouble();
+                   System.out.println("Result: " + sqrt(valueSqrt));
+                   break;
+               case "log":
+                   System.out.println("Enter a value:");
+                   double valueLog = scanner.nextDouble();
+                   System.out.println("Result: " + log(valueLog));
+                   break;
+               case "log10":
+                   System.out.println("Enter a value:");
+                   double valueLog10 = scanner.nextDouble();
+                   System.out.println("Result: " + log10(valueLog10));
+                   break;
+               case "sin":
+                   System.out.println("Enter a value(Degrees):");
+                   double valueSin = scanner.nextDouble();
+                   System.out.println("Result: " + sin(valueSin));
+                   break;
+               case "cos":
+                   System.out.println("Enter a value(Degrees):");
+                   double valueCos = scanner.nextDouble();
+                   System.out.println("Result: " + cos(valueCos));
+                   break;
+               case "tan":
+                   System.out.println("Enter a value(Degrees):");
+                   double valueTan = scanner.nextDouble();
+                   System.out.println("Result: " + tan(valueTan));
                    break;
                default:
                    System.out.println("Invalid operation");
+                   break;
            }
        }
        scanner.close();
@@ -135,15 +195,88 @@ public class Application {
        return a / b;
    }
    /**
-    *  does the factorial of the value
+    * does the factorial of the value
     * @param n the value
     * @return the factorial of n
     */
-   public static double factorial(double n) {
-       if (n <= 1) {
-           return 1;
-       } else {
-           return n * factorial(n - 1);
+   
+   public static double power(double base, double exponent) {
+       return Math.pow(base, exponent);
+   }
+
+   /**
+    * The square root of a value
+    * @param a The value
+    * @return The square root of a
+    */
+   public static double sqrt(double a) {
+       return Math.sqrt(a);
+   }
+
+   /**
+    * The natural logarithm of a value
+    * @param a The value
+    * @return The natural logarithm of a
+    */
+   public static double log(double a) {
+       return Math.log(a);
+   }
+
+   /**
+    *  The base 10 logarithm of a value
+    * @param a The value
+    * @return The base 10 logarithm of a
+    */
+   public static double log10(double a) {
+       return Math.log10(a);
+   }
+
+   /**
+    * The sine of a value
+    * @param a The value
+    * @return The sine of a in radians
+    */
+   public static double sin(double a) {
+       return Math.sin(a);
+   }
+
+   /**
+    * The cosine of a value
+    * @param a The value 
+    * @return The cosine of a in radians
+    */
+   public static double cos(double a) {
+       return Math.cos(a);
+   }
+
+   /**
+    * Calculates the tangent of a value
+    * @param a The value
+    * @return The tangent of a in radians
+    */
+   public static double tan(double a) {
+       return Math.tan(a);
+   }
+   // Factorial calculation with progress display
+   public static long factorial(int num) {
+       if (num < 0) {
+           System.out.println("Factorial of negative number is undefined.");
+           return 0;
        }
+       return factorialHelper(num, num);
+   }
+   // factorial function will call this function for the progress display
+   private static long factorialHelper(int originalNum, int num) {
+       if (num <= 1) {
+           System.out.print("\rCalculating factorial: 100%");
+           return 1;
+       }
+      
+       int progress = (int) (((originalNum - num + 1) / (double) originalNum) * 100);
+       System.out.print("\rCalculating factorial: " + progress + "%");
+       return num * factorialHelper(originalNum, num - 1);
+       
    }
 }
+
+
